6a8970e0ff426bb348913e7f26084972c96315b3
Luis Steidle <luis.steidle@frachtwerk.de>
2023-01-19 19:45:20 +0100

    feat(lib): add CommonFooter component

---
+++ packages/lib/src/components/common/Footer/CommonFooter.tsx
@@ -0,0 +1,30 @@
+import { Anchor, Flex, Footer, MediaQuery, Text } from '@mantine/core'
+import { IconCopyright } from '@tabler/icons'
+import { useTranslation } from 'react-i18next'
+
+function CommonFooter() {
+  const { t } = useTranslation()
+  return (
+    <Footer height={60} p="md">
+      <Flex
+        justify={{ base: 'center', xs: 'space-between' }}
+        direction="row"
+        wrap="wrap"
+      >
+        <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+          <Flex gap="xs" align="center" sx={{ marginLeft: '5px' }}>
+            <IconCopyright size="16" />
+            <Text>{t('footer.license')}</Text>
+          </Flex>
+        </MediaQuery>
+        <Flex direction="row" gap="xl">
+          <Anchor href="/">{t('footer.privacy')}</Anchor>
+          <Anchor href="/">{t('footer.imprint')}</Anchor>
+          <Anchor href="/">{t('footer.contact')}</Anchor>
+        </Flex>
+      </Flex>
+    </Footer>
+  )
+}
+
+export default CommonFooter
