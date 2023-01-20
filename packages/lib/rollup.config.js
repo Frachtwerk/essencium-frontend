f45b42c0cc5a378c1aa99cce2dc448d289290001
Gerrit Schröer <gerrit.schroeer@frachtwerk.de>
2023-01-20 15:51:40 +0100

    fix(lib): add '@tabler/icons' as external dependency

--- packages/lib/rollup.config.js
+++ packages/lib/rollup.config.js
@@ -36,6 +36,7 @@ export default [
       '@fortawesome/fontawesome-svg-core',
       '@fortawesome/free-solid-svg-icons',
       '@fortawesome/react-fontawesome',
+      '@tabler/icons',
     ],
   },
   {
