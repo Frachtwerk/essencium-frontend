0da57a22e1676aba38df1fba8af5c5cff6a1d4ca
Cathrin <Frachtwerk@Cathrins-MacBook-Pro.local>
2023-01-25 10:06:35 +0100

    feat: add PasswordChangeForm to ProfileView

---
+++ packages/app/src/components/ProfileView/PasswordChangeForm.tsx
@@ -0,0 +1,36 @@
+import { PasswordInput, Button } from '@mantine/core'
+import { IconCheck } from '@tabler/icons'
+
+export default function PasswordChangeForm() {
+  return (
+    <form>
+      <PasswordInput
+        placeholder="Ihr altes Passwort"
+        label="Altes Passwort"
+        radius="md"
+        maw="60%"
+        withAsterisk
+        mb="md"
+      />
+      <PasswordInput
+        placeholder="Ihr neues Passwort"
+        label="Neues Passwort"
+        radius="md"
+        maw="60%"
+        withAsterisk
+        mb="md"
+      />
+      <PasswordInput
+        placeholder="Wiederholen Sie Ihr neues Passwort"
+        label="Neues Passwort bestätigen"
+        radius="md"
+        mb="md"
+        maw="60%"
+        withAsterisk
+      />
+      <Button uppercase mt="md" leftIcon={<IconCheck />}>
+        Speichern
+      </Button>
+    </form>
+  )
+}
