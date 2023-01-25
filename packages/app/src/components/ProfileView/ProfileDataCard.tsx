019a4af80bbce3486d349fc7a4fcdf40c571cc30
Cathrin <Frachtwerk@Cathrins-MacBook-Pro.local>
2023-01-25 13:39:01 +0100

    feat(app): change text to variable

--- packages/app/src/components/ProfileView/ProfileDataCard.tsx
+++ packages/app/src/components/ProfileView/ProfileDataCard.tsx
@@ -1,19 +1,22 @@
 import { Card, Tabs } from '@mantine/core'
 import { IconLock, IconUser } from '@tabler/icons'
+import { useTranslation } from 'react-i18next'
 
 import PasswordChangeForm from './PasswordChangeForm'
 import PersonalDataForm from './PersonalDataForm'
 
 export default function ProfileDataCard() {
+  const { t, i18n } = useTranslation()
+
   return (
     <Card shadow="sm" p="lg" radius="md" withBorder>
       <Tabs defaultValue="personalDataForm">
         <Tabs.List>
           <Tabs.Tab value="personalDataForm" icon={<IconUser size={14} />}>
-            Persönliche Daten
+            {t('Persönliche Daten')}
           </Tabs.Tab>
           <Tabs.Tab value="passwordChange" icon={<IconLock size={14} />}>
-            Passwort ändern
+            {t('Passwort ändern')}
           </Tabs.Tab>
         </Tabs.List>
 
