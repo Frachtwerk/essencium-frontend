a2cc3bf50c97e0ea9a25caf610dce96cc8e76633
Cathrin <Frachtwerk@Cathrins-MacBook-Pro.local>
2023-01-26 09:54:13 +0100

    feat: add settings tab and content

--- packages/app/src/components/ProfileView/PasswordChangeForm.tsx
+++ packages/app/src/components/ProfileView/PasswordChangeForm.tsx
@@ -1,7 +1,9 @@
 import { Button, Flex, PasswordInput } from '@mantine/core'
 import { IconCheck } from '@tabler/icons'
+import { useTranslation } from 'react-i18next'
 
 export function PasswordChangeForm() {
+  const { t } = useTranslation()
   return (
     <form>
       <Flex
@@ -10,8 +12,14 @@ export function PasswordChangeForm() {
         align={{ base: 'center', xs: 'flex-start' }}
       >
         <PasswordInput
-          placeholder="Ihr altes Passwort"
-          label="Altes Passwort"
+          placeholder={
+            t(
+              'ProfileView.dataCard.tabs.passwordChange.content.currentPassword'
+            ) as string
+          }
+          label={t(
+            'ProfileView.dataCard.tabs.passwordChange.content.currentPassword'
+          )}
           radius="md"
           miw="60%"
           withAsterisk
@@ -19,8 +27,14 @@ export function PasswordChangeForm() {
         />
 
         <PasswordInput
-          placeholder="Ihr neues Passwort"
-          label="Neues Passwort"
+          placeholder={
+            t(
+              'ProfileView.dataCard.tabs.passwordChange.content.newPassword'
+            ) as string
+          }
+          label={t(
+            'ProfileView.dataCard.tabs.passwordChange.content.newPassword'
+          )}
           radius="md"
           miw="60%"
           withAsterisk
@@ -28,8 +42,14 @@ export function PasswordChangeForm() {
         />
 
         <PasswordInput
-          placeholder="Wiederholen Sie Ihr neues Passwort"
-          label="Neues Passwort bestätigen"
+          placeholder={
+            t(
+              'ProfileView.dataCard.tabs.passwordChange.content.newPassword'
+            ) as string
+          }
+          label={t(
+            'ProfileView.dataCard.tabs.passwordChange.content.newPassword'
+          )}
           radius="md"
           mb="md"
           miw="60%"
@@ -37,7 +57,7 @@ export function PasswordChangeForm() {
         />
 
         <Button mt="md" leftIcon={<IconCheck />}>
-          Passwort ändern
+          {t('ProfileView.dataCard.tabs.passwordChange.content.savePassword')}
         </Button>
       </Flex>
     </form>
