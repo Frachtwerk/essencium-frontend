b49b74b340afd55680062519cb265b95d95a7d35
Gerrit Schröer <gerrit.schroeer@frachtwerk.de>
2023-01-26 15:26:52 +0100

    refactor(app): simplify translations approach for now

--- packages/app/src/plugins/i18n.ts
+++ packages/app/src/plugins/i18n.ts
@@ -1,7 +1,5 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import ChainedBackend from 'i18next-chained-backend'
-import HttpBackend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 import { de, en } from 'translations'
 
@@ -9,36 +7,24 @@ i18n
   // detect user language
   .use(LanguageDetector)
 
-  // chain multiple resources
-  .use(ChainedBackend)
-
   // connect with React
   .use(initReactI18next)
 
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    // different namespaces to seperate backend and local translations
-    ns: ['backendTranslation', 'localTranslation'],
-    defaultNS: 'backendTranslation',
-    fallbackNS: 'localTranslation',
-
+    resources: {
+      en: {
+        translation: en,
+      },
+      de: {
+        translation: de,
+      },
+    },
     lng: 'en',
-
+    fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
-    },
-
-    backend: {
-      backends: [HttpBackend, HttpBackend],
-      backendOptions: [
-        {
-          loadPath: backendTranslations,
-        },
-        {
-          loadPath: localTranslations,
-        },
-      ],
     },
   })
 
