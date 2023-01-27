98866405ef2ac02ab9173956cffc049d6e68c9d8
Cathrin <cathrin.truchan@frachtwerk.de>
2023-01-27 14:38:30 +0100

    feat(app): add media query and change button style

--- packages/app/src/components/ProfileView/PasswordChangeForm.tsx
+++ packages/app/src/components/ProfileView/PasswordChangeForm.tsx
@@ -1,62 +1,64 @@
-import { Button, Flex, PasswordInput } from '@mantine/core'
-import { IconCheck } from '@tabler/icons'
+import { Button, Flex, MediaQuery, PasswordInput } from '@mantine/core'
 import { useTranslation } from 'react-i18next'
 
 export function PasswordChangeForm() {
   const { t } = useTranslation()
+
   return (
     <form>
-      <Flex
-        direction={{ base: 'column', xs: 'column' }}
-        gap={{ base: 'sm', xs: 'lg' }}
-        align={{ base: 'center', xs: 'flex-start' }}
-      >
-        <PasswordInput
-          placeholder={
-            t(
+      <Flex direction="column" align="flex-start">
+        <MediaQuery query="(max-width: 600px)" styles={{ minWidth: '100%' }}>
+          <PasswordInput
+            placeholder={
+              t(
+                'ProfileView.dataCard.tabs.passwordChange.content.currentPassword'
+              ) as string
+            }
+            label={t(
               'ProfileView.dataCard.tabs.passwordChange.content.currentPassword'
-            ) as string
-          }
-          label={t(
-            'ProfileView.dataCard.tabs.passwordChange.content.currentPassword'
-          )}
-          radius="md"
-          miw="60%"
-          withAsterisk
-          mb="md"
-        />
+            )}
+            radius="md"
+            miw="60%"
+            withAsterisk
+            mb="md"
+          />
+        </MediaQuery>
 
-        <PasswordInput
-          placeholder={
-            t(
+        <MediaQuery query="(max-width: 600px)" styles={{ minWidth: '100%' }}>
+          <PasswordInput
+            placeholder={
+              t(
+                'ProfileView.dataCard.tabs.passwordChange.content.newPassword'
+              ) as string
+            }
+            label={t(
               'ProfileView.dataCard.tabs.passwordChange.content.newPassword'
-            ) as string
-          }
-          label={t(
-            'ProfileView.dataCard.tabs.passwordChange.content.newPassword'
-          )}
-          radius="md"
-          miw="60%"
-          withAsterisk
-          mb="md"
-        />
+            )}
+            radius="md"
+            miw="60%"
+            withAsterisk
+            mb="md"
+          />
+        </MediaQuery>
 
-        <PasswordInput
-          placeholder={
-            t(
+        <MediaQuery query="(max-width: 600px)" styles={{ minWidth: '100%' }}>
+          <PasswordInput
+            placeholder={
+              t(
+                'ProfileView.dataCard.tabs.passwordChange.content.newPassword'
+              ) as string
+            }
+            label={t(
               'ProfileView.dataCard.tabs.passwordChange.content.newPassword'
-            ) as string
-          }
-          label={t(
-            'ProfileView.dataCard.tabs.passwordChange.content.newPassword'
-          )}
-          radius="md"
-          mb="md"
-          miw="60%"
-          withAsterisk
-        />
+            )}
+            radius="md"
+            mb="md"
+            miw="60%"
+            withAsterisk
+          />
+        </MediaQuery>
 
-        <Button mt="md" leftIcon={<IconCheck />}>
+        <Button mt="md" variant="light">
           {t('ProfileView.dataCard.tabs.passwordChange.content.savePassword')}
         </Button>
       </Flex>
