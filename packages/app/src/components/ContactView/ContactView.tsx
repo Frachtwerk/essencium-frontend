9be6ac49dfc2da0ff2a753b502974721e2444b19
Cathrin <cathrin.truchan@frachtwerk.de>
2023-01-30 12:58:34 +0100

    feat(app): add ContactView

---
+++ packages/app/src/components/ContactView/ContactView.tsx
@@ -0,0 +1,21 @@
+import { Grid } from '@mantine/core'
+
+import { ContactForm } from './ContactForm'
+import { ContactPersonCard } from './ContactPersonCard'
+
+type ContactPersonProps = {
+  contactPersonContent: JSX.Element
+}
+
+export function ContactView({ contactPersonContent }: ContactPersonProps) {
+  return (
+    <Grid>
+      <Grid.Col md={4}>
+        <ContactPersonCard contactPersonContent={contactPersonContent} />
+      </Grid.Col>
+      <Grid.Col md={8}>
+        <ContactForm />
+      </Grid.Col>
+    </Grid>
+  )
+}
