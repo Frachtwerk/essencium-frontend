31d8d3fac5f85092a2fb4dd77d1441f2f3c83a6b
Cathrin <cathrin.truchan@frachtwerk.de>
2023-02-02 13:14:53 +0100

    refactor(lib): move SetPassword to lib

---
+++ packages/lib/src/components/common/SetPassword/SetPasswordView.tsx
@@ -0,0 +1,11 @@
+import { Center } from '@mantine/core'
+
+import { SetPasswordForm } from './components/SetPassWordForm'
+
+export function SetPasswordView() {
+  return (
+    <Center w="100%" h="100%">
+      <SetPasswordForm />
+    </Center>
+  )
+}
