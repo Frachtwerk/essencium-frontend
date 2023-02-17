016c1486f5ee22a7b0dbc2f75549022260c6b553
qhantom <me@gerrit.tech>
2023-02-17 12:01:31 +0100

    refactor: use i18n for validation messages instead of zod

--- packages/lib/src/components/Login/components/LoginForm/types.ts
+++ packages/lib/src/components/Login/components/LoginForm/types.ts
@@ -1,4 +1,7 @@
+import { i18n } from 'translations'
 import { z } from 'zod'
+
+const { t } = i18n
 
 export type LoginCredentials = {
   email: string
@@ -7,7 +10,7 @@ export type LoginCredentials = {
 }
 
 export const loginFormSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8), // TODO: check which backend validation is used
+  email: z.string().email(String(t('validation.email.notValid'))),
+  password: z.string().min(8, String(t('validation.password.minLength'))), // TODO: check which backend validation is used
   rememberUser: z.boolean(),
 })
