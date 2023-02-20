3fe5ad7d45389bafac1199c8cc60f14b3f5ed107
Luis Steidle <luis@steidle.me>
2023-02-20 18:05:43 +0100

    feat: implement login functionality

--- packages/app/src/views/LoginView/LoginView.tsx
+++ packages/app/src/views/LoginView/LoginView.tsx
@@ -1,5 +1,22 @@
 import { Login, LoginForm } from 'lib'
+import { useEffect, useState } from 'react'
+
+import { useCreateToken } from '@/api/auth'
 
 export function LoginView(): JSX.Element {
-  return <Login form={<LoginForm />} />
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { mutate } = useCreateToken(username, password)
+
+  function handleLogin(name: string, pw: string): void {
+    setUsername(() => name)
+    setPassword(() => pw)
+  }
+
+  useEffect(() => {
+    if (username && password) mutate()
+  }, [username, password, mutate])
+
+  return <Login form={<LoginForm handleLogin={handleLogin} />} />
 }
