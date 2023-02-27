5b50cf6465f4f5eccfbeb074f50c04eee79e79df
qhantom <me@gerrit.tech>
2023-02-27 10:31:04 +0100

    chore: remove zustand

--- packages/app/src/store/slices/index.ts
+++
@@ -1,4 +0,0 @@
-export type { DinoSlice } from './dinoSlice'
-export { createDinoSlice } from './dinoSlice'
-export type { UserSlice } from './userSlice'
-export { createUserSlice } from './userSlice'
