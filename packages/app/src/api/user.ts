7c3b8868b1a7caca19a3aedac0701676ab5d36c4
Cathrin <cathrin.truchan@frachtwerk.de>
2023-03-02 14:16:38 +0100

    refactor: change baseURL

--- packages/app/src/api/user.ts
+++ packages/app/src/api/user.ts
@@ -2,9 +2,13 @@ import { useQuery, UseQueryResult } from '@tanstack/react-query'
 import { AxiosError } from 'axios'
 import { User } from 'lib'
 
-import { api } from '../api'
+import { api } from './api'
 
 export const useGetUsers = (): UseQueryResult<User[], AxiosError> =>
   useQuery(['users'], () =>
-    api.get<User[]>('/users').then(response => response.data)
+    api
+      .get<User[]>('/users', {
+        baseURL: 'https://jsonplaceholder.typicode.com',
+      })
+      .then(response => response.data)
   )
