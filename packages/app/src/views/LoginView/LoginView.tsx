8a0eb334518ccfa4682ced763f0be35ac9b574a2
qhantom <me@gerrit.tech>
2023-03-13 10:59:04 +0100

    fix: use onSuccess handler instead of useEffect hook

--- packages/app/src/views/LoginView/LoginView.tsx
+++ packages/app/src/views/LoginView/LoginView.tsx
@@ -1,24 +1,23 @@
 import { useNavigate } from '@tanstack/react-router'
 import { Login, LoginForm } from 'lib'
-import { useEffect } from 'react'
 
-import { tokenAtom, useCreateToken } from '@/api/auth'
-import { store } from '@/store'
+import { useCreateToken } from '@/api/auth'
 
 export function LoginView(): JSX.Element {
   const navigate = useNavigate()
 
   const { mutate } = useCreateToken()
 
-  const token = store.get(tokenAtom)
-
   function handleLogin(username: string, password: string): void {
-    mutate({ username, password })
+    mutate(
+      { username, password },
+      {
+        onSuccess: () => {
+          navigate({ to: '/' })
+        },
+      }
+    )
   }
-
-  useEffect(() => {
-    if (token) navigate({ to: '/' })
-  }, [token, navigate])
 
   return <Login form={<LoginForm handleLogin={handleLogin} />} />
 }
