e5a3ba05f5ef1855335b2a94a67e85447d21ec22
Luis Steidle <luis@steidle.me>
2023-03-20 15:47:39 +0100

    refactor: update route descriptions

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -41,35 +41,35 @@ export const NAV_LINKS: NavLink[] = [
     color: 'blue',
     label: 'navigation.home',
     to: '/',
-    description: 'Lorem Ipsum',
+    description: 'Homepage',
   },
   {
     icon: <IconUsers size={20} />,
     color: 'blue',
     label: 'navigation.users',
     to: '/users',
-    description: 'Lorem Ipsum',
+    description: 'Users',
   },
   {
     icon: <IconUserCheck size={20} />,
     color: 'blue',
     label: 'navigation.roles',
     to: '/roles',
-    description: 'Lorem Ipsum',
+    description: 'User Roles',
   },
   {
     icon: <IconShieldCheck size={20} />,
     color: 'blue',
     label: 'navigation.rights',
     to: '/rights',
-    description: 'Lorem Ipsum',
+    description: 'User Rights',
   },
   {
     icon: <IconLanguage size={20} />,
     color: 'blue',
     label: 'navigation.translations',
     to: '/translations',
-    description: 'Lorem Ipsum',
+    description: 'Translations',
   },
 ]
 
@@ -78,19 +78,19 @@ export const FOOTER_LINKS: FooterLink[] = [
     icon: <IconShieldLock size={20} />,
     label: 'footer.privacy',
     to: '/',
-    description: 'Lorem Ipsum',
+    description: 'Privacy',
   },
   {
     icon: <IconSectionSign size={20} />,
     label: 'footer.imprint',
     to: '/',
-    description: 'Lorem Ipsum',
+    description: 'Imprint',
   },
   {
     icon: <IconMessage size={20} />,
     label: 'footer.contact',
     to: '/contact',
-    description: 'Lorem Ipsum',
+    description: 'Contact',
   },
 ]
 
@@ -101,7 +101,7 @@ export const SEARCH_ITEMS: SearchItems[] = [
     icon: <IconUsers size={20} />,
     label: 'profileView.title',
     to: '/profile',
-    description: 'Lorem Ipsum',
+    description: 'User Profile',
   },
 ]
 
