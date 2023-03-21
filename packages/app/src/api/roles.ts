df4af7974b3742d06b893edc61f78fc054cfce51
Luis Steidle <luis@steidle.me>
2023-03-21 15:32:34 +0100

    feat: add roles query function

---
+++ packages/app/src/api/roles.ts
@@ -0,0 +1,42 @@
+import { useQuery, UseQueryResult } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
+import { UserRole } from 'lib'
+
+import { api } from './api'
+
+const VERSION = 'v1'
+
+// needs abstraction in API layer
+type UserRoleResponse = {
+  content: UserRole[]
+  empty: boolean
+  first: boolean
+  last: boolean
+  number: number
+  numberOfElements: number
+  pageable: {
+    offset: number
+    pageNumber: number
+    pageSize: number
+    paged: boolean
+    sort: {
+      empty: boolean
+      sorted: boolean
+      unsorted: boolean
+    }
+    unpaged: boolean
+  }
+  size: number
+  sort: {
+    empty: boolean
+    sorted: boolean
+    unsorted: boolean
+  }
+  totalElements: number
+  totalPages: number
+}
+
+export const useGetRoles = (): UseQueryResult<UserRoleResponse, AxiosError> =>
+  useQuery(['roles'], () =>
+    api.get<UserRole[]>(`${VERSION}/roles`).then(response => response.data)
+  )
