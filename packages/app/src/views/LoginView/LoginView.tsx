ba391d4172bda91f12c3c85c4ccb23b0d7071fa4
Gerrit Schröer <gerrit.schroeer@frachtwerk.de>
2023-03-22 11:50:02 +0100

    refactor: sort imports

--- packages/app/src/views/LoginView/LoginView.tsx
+++ packages/app/src/views/LoginView/LoginView.tsx
@@ -1,9 +1,9 @@
+import { showNotification } from '@mantine/notifications'
 import { useNavigate } from '@tanstack/react-router'
+import { AxiosError } from 'axios'
 import { Login, LoginForm } from 'lib'
 
 import { useCreateToken } from '@/api/auth'
-import { showNotification } from '@mantine/notifications'
-import { AxiosError } from 'axios'
 
 export function LoginView(): JSX.Element {
   const navigate = useNavigate()
