4958737ef6147c8168c4ec7a6ed34ae25c93ea7e
Cathrin <cathrin.truchan@frachtwerk.de>
2023-03-22 16:05:44 +0100

    chore: add translation

--- packages/lib/src/components/Profile/components/ProfileOverviewCard.tsx
+++ packages/lib/src/components/Profile/components/ProfileOverviewCard.tsx
@@ -1,10 +1,13 @@
 import { Avatar, Badge, Card, Flex, Text, Title } from '@mantine/core'
+import { useTranslation } from 'react-i18next'
 
 import { ProfileOverviewCardProps } from '../types'
 
 export function ProfileOverviewCard({
   user,
 }: ProfileOverviewCardProps): JSX.Element {
+  const { t } = useTranslation()
+
   return (
     <Card shadow="sm" p="lg" radius="sm" withBorder>
       <Flex gap="md" justify="center" align="center" direction="column">
@@ -22,7 +25,11 @@ export function ProfileOverviewCard({
 
         <Text>{user.role.name}</Text>
 
-        <Badge size="lg"> {user.enabled ? 'active' : 'inactive'} </Badge>
+        <Badge size="lg">
+          {user.enabled
+            ? t('profileView.overviewCard.active')
+            : t('profileView.overviewCard.inactive')}
+        </Badge>
       </Flex>
     </Card>
   )
