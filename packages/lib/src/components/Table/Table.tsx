d91b729820a32146ec7f1bdc6b3a7e00e4e49714
Luis Steidle <luis@steidle.me>
2023-04-12 14:56:02 +0200

    fix: add horizontal scroll to table

--- packages/lib/src/components/Table/Table.tsx
+++ packages/lib/src/components/Table/Table.tsx
@@ -9,62 +9,64 @@ type Props<T> = {
 export function Table<T>({ tableModel }: Props<T>): JSX.Element {
   return (
     <Flex direction="column" align="end">
-      <MantineTable striped highlightOnHover>
-        <thead>
-          {tableModel.getHeaderGroups().map(headerGroup => (
-            <tr key={headerGroup.id} data-testid="header-row">
-              {headerGroup.headers.map(header => (
-                <th key={header.id}>
-                  <Flex
-                    align="center"
-                    justify="space-between"
-                    sx={{ cursor: 'pointer' }}
-                    onClick={header.column.getToggleSortingHandler()}
-                  >
+      <div style={{ overflowX: 'auto', width: '100%' }}>
+        <MantineTable striped highlightOnHover>
+          <thead>
+            {tableModel.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id} data-testid="header-row">
+                {headerGroup.headers.map(header => (
+                  <th key={header.id}>
+                    <Flex
+                      align="center"
+                      justify="space-between"
+                      sx={{ cursor: 'pointer' }}
+                      onClick={header.column.getToggleSortingHandler()}
+                    >
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                      {
+                        {
+                          asc: <IconSortAscending2 />,
+                          desc: <IconSortDescending2 />,
+                        }[(header.column.getIsSorted() as string) ?? null]
+                      }
+                    </Flex>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+
+          <tbody>
+            {tableModel.getRowModel().rows.map(row => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map(cell => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+
+          <tfoot>
+            {tableModel.getFooterGroups().map(footerGroup => (
+              <tr key={footerGroup.id}>
+                {footerGroup.headers.map(header => (
+                  <th key={header.id}>
                     {flexRender(
-                      header.column.columnDef.header,
+                      header.column.columnDef.footer,
                       header.getContext()
                     )}
-                    {
-                      {
-                        asc: <IconSortAscending2 />,
-                        desc: <IconSortDescending2 />,
-                      }[(header.column.getIsSorted() as string) ?? null]
-                    }
-                  </Flex>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-
-        <tbody>
-          {tableModel.getRowModel().rows.map(row => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map(cell => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-
-        <tfoot>
-          {tableModel.getFooterGroups().map(footerGroup => (
-            <tr key={footerGroup.id}>
-              {footerGroup.headers.map(header => (
-                <th key={header.id}>
-                  {flexRender(
-                    header.column.columnDef.footer,
-                    header.getContext()
-                  )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tfoot>
-      </MantineTable>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </tfoot>
+        </MantineTable>
+      </div>
     </Flex>
   )
 }
