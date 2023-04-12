c623e1754cd25fbe76fbcccf33ebd58230783752
Luis Steidle <luis@steidle.me>
2023-04-12 15:14:23 +0200

    refactor: add page and size params to role request

--- packages/app/src/views/ProfileView/ProfileView.tsx
+++ packages/app/src/views/ProfileView/ProfileView.tsx
@@ -15,7 +15,7 @@ export function ProfileView(): JSX.Element {
 
   const { mutate: updatePassword } = useUpdatePassword()
 
-  const { data: rolesRequest } = useGetRoles()
+  const { data: rolesRequest } = useGetRoles(0, 9999)
 
   const roles = rolesRequest?.content || []
 
