f99429e9d0ad10aa93da104cdb01d00a65e93720
Luis Steidle <luis@steidle.me>
2023-04-12 13:32:12 +0200

    feat: add pagination and table model to roles view

--- packages/app/src/views/RolesView/RolesView.tsx
+++ packages/app/src/views/RolesView/RolesView.tsx
@@ -1,9 +1,26 @@
 /* eslint-disable react/no-unstable-nested-components */
-import { Badge, Button, Center, Flex, Loader, Text, Title } from '@mantine/core'
+import {
+  Badge,
+  Button,
+  Center,
+  Flex,
+  Loader,
+  Pagination,
+  Select,
+  Text,
+  Title,
+} from '@mantine/core'
 import { IconUserCheck } from '@tabler/icons-react'
-import { ColumnDef } from '@tanstack/react-table'
-import { HttpNotification, Roles } from 'lib'
-import { useCallback, useMemo } from 'react'
+import {
+  ColumnDef,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from '@tanstack/react-table'
+import { HttpNotification, Table } from 'lib'
+import { useCallback, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { RightOutput, RoleOutput } from 'types'
 
@@ -11,6 +28,10 @@ import { useGetRoles } from '@/api/roles'
 
 export function RolesView(): JSX.Element {
   const { t } = useTranslation()
+
+  const [activePage, setActivePage] = useState(1)
+  const [pageSize, setPageSize] = useState(20)
+  const [sorting, setSorting] = useState<SortingState>([])
 
   const {
     data: roles,
@@ -20,7 +41,7 @@ export function RolesView(): JSX.Element {
     isError: isErrorRoles,
     error: errorRoles,
     refetch: refetchRoles,
-  } = useGetRoles()
+  } = useGetRoles(activePage - 1, pageSize)
 
   const handleRefetch = useCallback((): void => {
     refetchRoles()
@@ -58,6 +79,19 @@ export function RolesView(): JSX.Element {
       },
     ]
   }, [t])
+
+  const table = useReactTable({
+    data: roles?.content || [],
+    columns,
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    pageCount: roles?.totalPages,
+  })
 
   return (
     <>
@@ -101,7 +135,42 @@ export function RolesView(): JSX.Element {
           <Loader size="xl" name="loader" />
         </Center>
       ) : (
-        <Roles roles={roles?.content || []} columns={columns} />
+        <>
+          <Table tableModel={table} />
+
+          <Flex align="center">
+            <Text size="sm" mr="xl">
+              {t('table.footer.pageCount')} {table.getPageCount()}
+            </Text>
+
+            <Flex align="center" mr="xl">
+              <Text size="sm" mr="xs">
+                {t('table.footer.pageSize')}
+              </Text>
+
+              <Select
+                defaultValue={String(pageSize)}
+                data={['10', '20', '30', '40', '50', '100']}
+                w={70}
+                onChange={e => {
+                  table.setPageSize(Number(e))
+                  setPageSize(Number(e))
+                  setActivePage(1)
+                  handleRefetch()
+                }}
+              />
+            </Flex>
+
+            <Pagination
+              total={table.getPageCount()}
+              value={activePage}
+              onChange={e => {
+                setActivePage(e)
+                handleRefetch()
+              }}
+            />
+          </Flex>
+        </>
       )}
     </>
   )
