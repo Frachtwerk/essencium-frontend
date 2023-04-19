a884c34fbaf65cf50d2e615608bb48ebd3d7c8ff
Gerrit Schröer <gerrit.schroeer@frachtwerk.de>
2023-04-19 16:37:47 +0200

    refactor: create user form component that gets used by add + update view

--- packages/app/src/views/UsersView/AddUserView.tsx
+++ packages/app/src/views/UsersView/AddUserView.tsx
@@ -1,17 +1,19 @@
 import { Card, Flex, Text, Title } from '@mantine/core'
 import { IconUserPlus } from '@tabler/icons-react'
 import { useNavigate } from '@tanstack/react-router'
-import { AddUser } from 'lib'
+import { UserForm } from 'lib'
 import { useTranslation } from 'react-i18next'
 import { UserInput } from 'types'
 
-import { useAddUser } from '@/api'
+import { useCreateUser } from '@/api'
 import { useGetRoles } from '@/api/roles'
 
 export function AddUserView(): JSX.Element {
   const { t } = useTranslation()
+
   const navigate = useNavigate()
-  const { mutate: addUser } = useAddUser()
+
+  const { mutate: addUser } = useCreateUser()
 
   const { data: rolesRequest } = useGetRoles({
     page: 0,
@@ -39,7 +41,11 @@ export function AddUserView(): JSX.Element {
       </Title>
 
       <Card shadow="sm" p="lg" radius="sm" withBorder maw="81.25rem">
-        <AddUser roles={roles} handleAddUser={handleAddUser} />
+        <UserForm
+          title={t('addUserView.form.userDataHeading')}
+          roles={roles}
+          handleForm={handleAddUser}
+        />
       </Card>
     </>
   )
