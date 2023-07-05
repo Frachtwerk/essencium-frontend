0ff4cc92a6f37880f76c6ad913328b548185ae86
Gerrit Schröer <gerrit.schroeer@frachtwerk.de>
2023-07-05 15:45:29 +0200

    test: get elements by label

--- packages/app/playwright/auth.setup.ts
+++ packages/app/playwright/auth.setup.ts
@@ -6,10 +6,10 @@ const authFile = './playwright/.auth/user.json'
 
 setup('authenticate', async ({ page }) => {
   await page.goto(`${BASE_URL}/login`)
-  await page.getByPlaceholder('E-Mail').click()
-  await page.getByPlaceholder('E-Mail').fill(ADMIN.username)
-  await page.getByPlaceholder('Password').click()
-  await page.getByPlaceholder('Password').fill(ADMIN.password)
+  await page.getByLabel('E-Mail').click()
+  await page.getByLabel('E-Mail').fill(ADMIN.username)
+  await page.getByLabel('Password').click()
+  await page.getByLabel('Password').fill(ADMIN.password)
   await page.getByRole('button', { name: 'Login' }).click()
 
   await page.waitForURL(BASE_URL)
