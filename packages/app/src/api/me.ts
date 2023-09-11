18fb14ccd041441eeea3fcf6c9b9ca206c028a02
Philipp <philipp.kaiser@frachtwerk.de>
2023-09-11 18:14:34 +0200

    chore(app): add user rights to store

--- packages/app/src/api/me.ts
+++ packages/app/src/api/me.ts
@@ -20,6 +20,7 @@
 import { withBaseStylingShowNotification } from '@frachtwerk/essencium-lib'
 import {
   PasswordChange,
+  RightOutput,
   UserOutput,
   UserUpdate,
 } from '@frachtwerk/essencium-types'
@@ -38,11 +39,13 @@ import { authTokenAtom } from '@/api/auth'
 import { api } from './api'
 
 export const userAtom = atomWithStorage<UserOutput | null>('user', null)
+export const userRightsAtom = atomWithStorage<string[] | null>('rights', null)
 
 export function useGetMe(): UseQueryResult<UserOutput, unknown> {
   const store = useStore()
 
   const [, setUser] = useAtom(userAtom)
+  const [, setUserRights] = useAtom(userRightsAtom)
   const authToken = useAtomValue(authTokenAtom)
 
   const query = useQuery({
@@ -53,6 +56,8 @@ export function useGetMe(): UseQueryResult<UserOutput, unknown> {
     onSuccess(data: UserOutput) {
       setUser(data)
       store.set(userAtom, data)
+
+      setUserRights(data?.role.rights.map(right => right.authority))
     },
   })
 
