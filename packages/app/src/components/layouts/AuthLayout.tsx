525ab8a00bcfc205d3b146d1d0749ccabe23b37f
Philipp <philipp.kaiser@frachtwerk.de>
2023-09-15 10:38:35 +0200

    Revert "chore(app): redirect if rights are missing"
    This reverts commit 820fc9b6a6e8680db676cbcf85405e8715a762f4.

--- packages/app/src/components/layouts/AuthLayout.tsx
+++ packages/app/src/components/layouts/AuthLayout.tsx
@@ -186,18 +186,6 @@ function AuthLayout({ children, routeName }: Props): JSX.Element | null {
     if (!authToken) router.push('/login')
   }, [user, router])
 
-  useEffect(() => {
-    const requiredRights = NAV_LINKS.find(
-      link => link.to === router.pathname
-    )?.rights
-
-    if (
-      !requiredRights ||
-      !requiredRights?.some(right => userRights?.includes(right))
-    )
-      router.push('/')
-  }, [userRights, router])
-
   function handleLogout(): void {
     logout()
 
