23756614ae0b00334696905375260f9b662aaef0
Philipp <philipp.kaiser@frachtwerk.de>
2023-09-28 18:24:50 +0200

    chore: wip [ci-skip]

--- packages/app/src/pages/404.tsx
+++ packages/app/src/pages/404.tsx
@@ -1,18 +1,13 @@
-import { useGetMe } from '@frachtwerk/essencium-lib'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 export default function Custom404(): null {
   const router = useRouter()
 
-  const { data: user } = useGetMe()
-
   useEffect(() => {
-    router.replace('/', undefined, {
-      locale: user?.locale,
-    })
+    router.replace('/')
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [router.asPath])
+  }, [router])
 
   return null
 }
