bb38a0018b39f7d5155c89a4684623f8e9047043
Philipp <philipp.kaiser@frachtwerk.de>
2023-09-29 09:29:08 +0200

    chore: wip [ci-skip]

--- packages/app/src/pages/404.tsx
+++ packages/app/src/pages/404.tsx
@@ -4,19 +4,12 @@ import { useEffect } from 'react'
 export default function Custom404(): null {
   const router = useRouter()
 
-  function sendHome(): void {
-    router.push('/')
-  }
-
   useEffect(() => {
     // router.back()
-    // router.events.on('routeChangeComplete', sendHome)
 
     router.push('/')
     // router.replace('/')
-    /* return () => {
-      router.events.off('routeChangeComplete', sendHome)
-    } */
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
