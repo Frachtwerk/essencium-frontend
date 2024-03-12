bc45632157f91bd4bfcf54b3958d1f0b0dd7e183
Gerrit Schröer <me@gerrit.tech>
2024-03-12 12:02:18 +0100

    test: set resolve alias for types package

--- packages/lib/vitest.config.ts
+++ packages/lib/vitest.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'happy-dom',
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@frachtwerk/essencium-types',
+        replacement: path.resolve(__dirname, '../types/src/index.ts'),
+      },
+    ],
   },
 })
