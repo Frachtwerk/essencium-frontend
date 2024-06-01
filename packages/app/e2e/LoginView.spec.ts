3971f87d6eb85c9733ec2112e084e1c6626e74d8
Gerrit Schröer <me@gerrit.tech>
2024-06-01 13:15:48 +0200

    fix: remove buggy no auth storage stage

--- packages/app/e2e/LoginView.spec.ts
+++ packages/app/e2e/LoginView.spec.ts
@@ -2,8 +2,6 @@ import { expect, test } from '@playwright/test'
 
 import { ADMIN, BASE_URL } from '../playwright.config'
 
-test.use({ storageState: './playwright/.auth/noAuth.json' })
-
 test('LoginView > redirect to login page', async ({ page }) => {
   await page.goto(BASE_URL)
   await expect(page).toHaveURL(`${BASE_URL}/login`)
